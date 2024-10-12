65709112cfae10a90d802852218366a016413a35
seoyounghan <happyunbd364@naver.com>
2024-10-12 17:04:07 +0900

    project setting

---
+++ AppName/AppName/AppNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNameApp.swift
+//  AppName
+//
+//  Created by hanseoyoung on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
