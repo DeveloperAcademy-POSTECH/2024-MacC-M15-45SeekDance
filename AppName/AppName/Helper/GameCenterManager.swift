f4b3dfbb0180e659a0067e6fa8b10de0d69b4c34
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-10-24 16:51:53 +0900

    #8 feat: set leaderboardID as string

--- AppName/AppName/Helper/GameCenterManager.swift
+++ AppName/AppName/Helper/GameCenterManager.swift
@@ -9,6 +9,8 @@ import Foundation
 import GameKit
 
 class GameCenterManager: NSObject {
+    let leaderboardID: String = "leaderboard1"
+    
     // 게임 센터 계정 인증하기
     func authenticateUser(){
         GKLocalPlayer.local.authenticateHandler = { vc, error in
@@ -23,22 +25,11 @@ class GameCenterManager: NSObject {
     // 리더보드 점수 업데이트 하기
     func submitPoint(point: Int){
         GKLeaderboard.submitScore(point, context: 0, player: GKLocalPlayer.local,
-            leaderboardIDs: ["leaderboard1"]) { error in
+            leaderboardIDs: [leaderboardID]) { error in
             if error != nil {
                 print("Error: \(error!.localizedDescription).")
             }
         }
         print("game center: updated leaderboard")
     }
-    
-    // 리더보드 가져오기
-    // 그러나 리더보드 전체를 가져와서 특정 값을 가져올 수 있을지는 모르겠다
-//    func fetchLeaderboard(){
-//        GKLeaderboard.loadLeaderboards(IDs: ["leaderboard1"], completionHandler: { leaderboards, error in
-//            if error != nil {
-//                print("Error: \(error!.localizedDescription).")
-//            } else {                print("game center: fetched leaderboard")
-//            }
-//        })
-//    }
 }
