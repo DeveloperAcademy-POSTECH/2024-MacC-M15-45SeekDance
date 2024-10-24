5cc43e8fd4d600b2eee2085eb9967531af2c7449
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-10-24 14:17:41 +0900

    #8 feat: create GameCenterManager and authenticate user in AuthenticateView

--- AppName/AppName/View/AuthenticateView.swift
+++ AppName/AppName/View/AuthenticateView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct AuthenticateView: View {
+    let gameCenterManager = GameCenterManager()
     @State private var point = 0
     var body: some View {
-        Text("your point: \(point)")
-        Button("Temporary tagging") {
-            point += 78
+        VStack {
+            Button(action: {
+                // 게임 센터로 이동
+            }, label: {
+                Image(systemName: "gamecontroller")
+                    .font(.title)
+            })
+            .buttonStyle(.bordered)
+            Spacer()
+            Text("your point: \(point)")
+            Spacer()
+            Button("Temporary tagging") {
+                // 포인트 부여 후 리더보드 수정
+                point += 78
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .buttonStyle(.borderedProminent)
+    }
+    
+    init() {
+        // 사용자 게임 센터 인증
+        gameCenterManager.authenticateUser()
     }
 }
 
