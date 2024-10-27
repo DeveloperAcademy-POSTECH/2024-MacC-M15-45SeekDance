14879307eded2fadfdf4a52a6d68f9f630432fa5
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-10-27 12:25:27 +0900

    #8 feat: set achievements when submit scores

--- AppName/AppName/Helper/GameCenterManager.swift
+++ AppName/AppName/Helper/GameCenterManager.swift
@@ -25,11 +25,31 @@ class GameCenterManager: NSObject {
     // 리더보드 점수 업데이트 하기
     func submitPoint(point: Int){
         GKLeaderboard.submitScore(point, context: 0, player: GKLocalPlayer.local,
-            leaderboardIDs: [leaderboardID]) { error in
+                                  leaderboardIDs: [leaderboardID]) { error in
             if error != nil {
                 print("Error: \(error!.localizedDescription).")
             }
         }
         print("game center: updated leaderboard")
+        // 78계단만 태깅할 때
+        reportAchievement(achievementID: "testfirst78staircase", isFirst: true)
+        reportAchievement(achievementID: "test78staircase")
+    }
+    
+    // 성취 업데이트하기
+    func reportAchievement(achievementID: String, isFirst: Bool = false){
+        let achievement = GKAchievement(identifier: achievementID)
+        if isFirst {
+            achievement.percentComplete = 100.0
+        } else {
+            achievement.percentComplete = 4.0
+        }
+        GKAchievement.report([achievement], withCompletionHandler: {(error: Error?) in
+            if error != nil {
+                // Handle the error that occurs.
+                print("Error: \(String(describing: error))")
+            }
+        })
+        print("game center: updated achievement")
     }
 }
