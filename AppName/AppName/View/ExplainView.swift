f24e77a7d9e0dcf57e3e3cbd4890bcd215be46b4
heexohee <rsoy2918@naver.com>
2024-10-30 15:13:19 +0900

    #30 NFC 설명창 수, List 컴포넌트 삭제하고, 커스텀 함

--- AppName/AppName/View/ExplainView.swift
+++ AppName/AppName/View/ExplainView.swift
@@ -14,50 +14,72 @@ struct ExplainView: View {
     var body: some View {
         NavigationView {
             ZStack(alignment: .topTrailing) {
-                // background
-                Color.white.ignoresSafeArea()
+                // Background
+                Color.back.ignoresSafeArea()
                 
                 VStack {
-                    Section {
-                        List {
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Group {
                             Text("1. 78 계단을 오른다.")
+                            Divider()
                             Text("2. 앱의 NFC 태깅하기 버튼을 누른다.")
-                            
-                            VStack {
+                            Divider()
+                            VStack(alignment: .leading, spacing: 5) {
                                 Text("3. 계단 중간에 위치한 [ㅇㅇㅇ]를 스캔해 주세요.")
                                 Text("휴대폰 위쪽이 [대상체 이름]을 스캔하고 있는지 확인해주세요.")
                                     .font(.footnote)
                                     .foregroundColor(.gray)
-                                Image("IMG1")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(maxWidth: 220)
-                                    .cornerRadius(12)
+                                
+                                HStack {
+                                    Spacer()
+                                    Image("IMG1")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(maxHeight: 320)
+                                        .cornerRadius(12)
+                                    Spacer()
+                                }
                             }
+                            
                         }
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
                     }
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.gray.opacity(0.3), lineWidth: 1)
+                            .background(RoundedRectangle(cornerRadius: 12).fill(Color.white))
+                    )
+                    .cornerRadius(10)
+                    .padding(.horizontal)
                     
-                    // 오류 신고 버튼
+                    Spacer()
+                    
+                    // 오류 문의 버튼
                     Button(action: {
                         // 오류 신고 action
                     }) {
-                        Text("오류 신고")
+                        Text("오류 문의")
                             .font(.body)
                             .fontWeight(.regular)
                             .foregroundColor(.indigo)
-                            .padding(.top, 10)
                     }
+                    .padding(.horizontal)
+                    .padding(.bottom, 88)
                 } // VStack
+                .padding(.top)
+                
                 // 닫기 버튼
-                .navigationTitle("NFC 태깅 방법")
-                .navigationBarTitleDisplayMode(.large)
+                .navigationTitle("도움이 필요하신가요?")
+                .navigationBarTitleDisplayMode(.inline)
                 .navigationBarItems(
                     trailing: Button(action: {
                         presentationMode.wrappedValue.dismiss()
                     }) {
                         Image(systemName: "x.circle.fill")
-                            .foregroundColor(.secondary)
-                            .padding(.top, 20)
+                            .foregroundColor(.gray)
+                            .frame(width: 30, height: 30)
                     }
                 )
             } // ZStack
@@ -65,8 +87,6 @@ struct ExplainView: View {
     }
 }
 
-
 #Preview {
     ExplainView()
 }
-
