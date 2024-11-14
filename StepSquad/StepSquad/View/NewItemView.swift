cc40e8e6b3ac909512f0a72a1529c0f448e5082b
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-11-14 21:54:08 +0900

    #75 feat: create NewItemView

---
+++ StepSquad/StepSquad/View/NewItemView.swift
@@ -0,0 +1,22 @@
+//
+//  NewItemView.swift
+//  StepSquad
+//
+//  Created by Groo on 11/14/24.
+//
+
+import SwiftUI
+
+struct NewItemView: View {
+    var body: some View {
+        Text("N")
+            .foregroundStyle(.white)
+            .padding(8)
+            .background(.red)
+            .clipShape(.circle)
+    }
+}
+
+#Preview {
+    NewItemView()
+}
