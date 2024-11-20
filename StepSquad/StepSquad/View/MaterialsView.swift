77394cbae1cfa526877ee9c76ae678fa01b49af0
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-11-20 11:22:59 +0900

    #93 feat: show level in MaterialsView

--- StepSquad/StepSquad/View/MaterialsView.swift
+++ StepSquad/StepSquad/View/MaterialsView.swift
@@ -22,18 +22,25 @@ struct MaterialsView: View {
                         if completedLevels.lastUpdatedLevel > 0 { // 획득한 약재가 있을 때
                             ForEach(1...completedLevels.lastUpdatedLevel, id: \.self) { level in
                                 HStack(spacing: 0) {
-                                    Image(levels[level - 1].itemImage)
+                                    Image(levels[level - 1].itemImage) // 약재 이미지
                                         .resizable()
                                         .aspectRatio(1, contentMode: .fit)
                                         .frame(width: 42, height: 42)
                                         .padding(.trailing, 0)
+                                    
+                                    Text("레벨 \(level)") // 레벨 표시
+                                        .font(.system(size: 12))
+                                        .foregroundStyle(getDifficultyColor(difficulty: levels[level - 1].difficulty))
+                                        .padding(4)
+                                        .background(getDifficultyPaleColor(difficulty: levels[level - 1].difficulty), in: RoundedRectangle(cornerRadius: 4))
+                                        .padding(.horizontal, 8)
 
-                                    Text(" \(levels[level - 1].item)")
+                                    Text(" \(levels[level - 1].item)") // 약재 이름
                                         .font(.system(size: 17))
 
                                     Spacer()
 
-                                    Text(completedLevels.getCompletedDateString(level: level))
+                                    Text(completedLevels.getCompletedDateString(level: level)) // 레벨 달성 날짜
                                         .font(.system(size: 15))
                                         .foregroundStyle(Color(hex: 0x3C3C43))
                                         .opacity(0.6)
