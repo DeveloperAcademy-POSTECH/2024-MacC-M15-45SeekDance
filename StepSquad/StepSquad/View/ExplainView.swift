c60bf57749f6cb598aaa6307f1d8de83cbcfb07d
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-11-20 10:51:04 +0900

    #93 feat: change explain about nfc tagging and leaderboard's score

--- StepSquad/StepSquad/View/ExplainView.swift
+++ StepSquad/StepSquad/View/ExplainView.swift
@@ -27,6 +27,15 @@ struct ExplainView: View {
                                                           trailing: 8))
 
                         })
+                        
+                        DisclosureGroup("NFC는 어디에 있나요?", content: {
+                                Text("태깅할 수 있는 NFC는 총 세 개로 아래 장소에 부착되어 있습니다.\n◦ 포스텍 78계단의 중간 지점 난간\n◦ C5 1-5층 계단 사이 난간\n◦ C5 5-6층 계단 사이 난간")
+                                .listRowInsets(EdgeInsets(top: 11,
+                                                          leading: 8,
+                                                          bottom: 11,
+                                                          trailing: 8))
+
+                        })
 
                         DisclosureGroup("획득 재료는 무엇인가요?", content: {
                             VStack(spacing: 0) {
@@ -48,7 +57,7 @@ struct ExplainView: View {
                         
                         DisclosureGroup("나의 순위는 어떻게 결정되나요?", content: {
                             VStack(spacing: 0) {
-                                Text("• 나의 순위 (칸) = 헬스킷의 권한을 허용한 이후부터 오른 층계(Flights Climbed) x 16 + NFC 태깅으로 얻은 특별 점수\n• 태깅할 수 있는 NFC는 총 세 개로 포스텍 78계단의 가운데, C5 1-5층 계단의 사이에, 5-6층 계단 사이 난간에 부착되어 있습니다.")
+                                Text("• 계단의 '칸'을 단위로 사용합니다.\n• 건강(Health)의 권한을 허용한 이후부터 오른 층계(Flights Climbed)의 한 층당 16칸, 그리고 NFC 태깅으로 얻은 특별 점수(칸)을 합산합니다.")
                             }.listRowInsets(EdgeInsets(top: 11,
                                                        leading: 8,
                                                        bottom: 24,
