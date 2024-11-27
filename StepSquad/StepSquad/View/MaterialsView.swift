7287d8626990fc34e8ecc40362a99f14af7b15cb
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-11-27 11:38:06 +0900

    #111 feat: add EmptyItemView with test var

--- StepSquad/StepSquad/View/MaterialsView.swift
+++ StepSquad/StepSquad/View/MaterialsView.swift
@@ -11,6 +11,7 @@ struct MaterialsView: View {
     @Binding var isMaterialSheetPresented: Bool
     @Binding var isShowingNewItem: Bool
     var completedLevels: CompletedLevels
+    var isEmpty = true
     
     var body: some View {
         NavigationView {
@@ -18,69 +19,73 @@ struct MaterialsView: View {
                 Color.backgroundColor.ignoresSafeArea()
                 
                 VStack() {
-                    List {
-                        if completedLevels.lastUpdatedLevel >= 0 { // 획득한 약재가 있을 때
-                            if completedLevels.isCompleted(level: 0) { // 불로초를 획득했다면
-                                HStack(spacing: 0) {
-                                    Image(levels[0]!.itemImage) // 약재 이미지
-                                        .resizable()
-                                        .aspectRatio(1, contentMode: .fit)
-                                        .frame(width: 42, height: 42)
-                                        .padding(.trailing, 0)
-                                    
-                                    Text("NFC") // 레벨 표시
-                                        .font(.system(size: 12))
-                                        .foregroundStyle(.white)
-                                        .padding(4)
-                                        .background(getDifficultyPaleColor(difficulty: levels[0]!.difficulty), in: RoundedRectangle(cornerRadius: 4))
-                                        .padding(.horizontal, 8)
-                                    
-                                    Text(" \(levels[0]!.item)") // 약재 이름
-                                        .font(.system(size: 17))
-                                    
-                                    Spacer()
-                                    
-                                    Text(completedLevels.getCompletedDateString(level: 0)) // 레벨 달성 날짜
-                                        .font(.system(size: 15))
-                                        .foregroundStyle(Color(hex: 0x3C3C43))
-                                        .opacity(0.6)
-                                }
-                                .padding(.horizontal, 4)
-                            }
-
-                            ForEach(0...completedLevels.lastUpdatedLevel, id: \.self) { level in
-                                if level > 0 {
+                    if isEmpty {
+                        EmptyItemView()
+                    } else {
+                        List {
+                            if completedLevels.lastUpdatedLevel >= 0 { // 획득한 약재가 있을 때
+                                if completedLevels.isCompleted(level: 0) { // 불로초를 획득했다면
                                     HStack(spacing: 0) {
-                                        Image(levels[level]!.itemImage) // 약재 이미지
+                                        Image(levels[0]!.itemImage) // 약재 이미지
                                             .resizable()
                                             .aspectRatio(1, contentMode: .fit)
                                             .frame(width: 42, height: 42)
                                             .padding(.trailing, 0)
                                         
-                                        Text("레벨 \(level)") // 레벨 표시
+                                        Text("NFC") // 레벨 표시
                                             .font(.system(size: 12))
-                                            .foregroundStyle(getDifficultyColor(difficulty: levels[level]!.difficulty))
+                                            .foregroundStyle(.white)
                                             .padding(4)
-                                            .background(getDifficultyPaleColor(difficulty: levels[level]!.difficulty), in: RoundedRectangle(cornerRadius: 4))
+                                            .background(getDifficultyPaleColor(difficulty: levels[0]!.difficulty), in: RoundedRectangle(cornerRadius: 4))
                                             .padding(.horizontal, 8)
                                         
-                                        Text(" \(levels[level]!.item)") // 약재 이름
+                                        Text(" \(levels[0]!.item)") // 약재 이름
                                             .font(.system(size: 17))
                                         
                                         Spacer()
                                         
-                                        Text(completedLevels.getCompletedDateString(level: level)) // 레벨 달성 날짜
+                                        Text(completedLevels.getCompletedDateString(level: 0)) // 레벨 달성 날짜
                                             .font(.system(size: 15))
                                             .foregroundStyle(Color(hex: 0x3C3C43))
                                             .opacity(0.6)
                                     }
                                     .padding(.horizontal, 4)
                                 }
+                                
+                                ForEach(0...completedLevels.lastUpdatedLevel, id: \.self) { level in
+                                    if level > 0 {
+                                        HStack(spacing: 0) {
+                                            Image(levels[level]!.itemImage) // 약재 이미지
+                                                .resizable()
+                                                .aspectRatio(1, contentMode: .fit)
+                                                .frame(width: 42, height: 42)
+                                                .padding(.trailing, 0)
+                                            
+                                            Text("레벨 \(level)") // 레벨 표시
+                                                .font(.system(size: 12))
+                                                .foregroundStyle(getDifficultyColor(difficulty: levels[level]!.difficulty))
+                                                .padding(4)
+                                                .background(getDifficultyPaleColor(difficulty: levels[level]!.difficulty), in: RoundedRectangle(cornerRadius: 4))
+                                                .padding(.horizontal, 8)
+                                            
+                                            Text(" \(levels[level]!.item)") // 약재 이름
+                                                .font(.system(size: 17))
+                                            
+                                            Spacer()
+                                            
+                                            Text(completedLevels.getCompletedDateString(level: level)) // 레벨 달성 날짜
+                                                .font(.system(size: 15))
+                                                .foregroundStyle(Color(hex: 0x3C3C43))
+                                                .opacity(0.6)
+                                        }
+                                        .padding(.horizontal, 4)
+                                    }
+                                }
                             }
                         }
+                        .scrollContentBackground(.hidden)
+                        .background(.clear)
                     }
-                    .scrollContentBackground(.hidden)
-                    .background(.clear)
                     
                     Text("주의: 계단사랑단 앱은 게임입니다. 실제 의학적 참고, 권고, 진단등을 위해 앱을 활용하면 안 됩니다.")
                         .font(.system(size: 15))
