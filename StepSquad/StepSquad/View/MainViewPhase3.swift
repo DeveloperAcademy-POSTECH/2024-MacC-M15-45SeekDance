6eeee1f603412d973cff56d1918d1480635cad23
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-11-28 17:41:00 +0900

    #111 test: 오늘까지 계단 걷기로 리더보드 점수 없데이트

--- StepSquad/StepSquad/View/MainViewPhase3.swift
+++ StepSquad/StepSquad/View/MainViewPhase3.swift
@@ -492,11 +492,11 @@ struct MainViewPhase3: View {
         let weeklyNfcPoint = weeklyScore(from: stairSteps)
         service.getWeeklyStairDataAndSave()
         let weeklyStairPoint = service.weeklyFlightsClimbed * 16
-        //        print("이번주 걸은 층계 * 16: \(weeklyStairPoint), nfc 점수: \(weeklyNfcPoint)")
+//                print("이번주 걸은 층계 * 16: \(weeklyStairPoint), nfc 점수: \(weeklyNfcPoint)")
         // TODO: - 에러 이후 변경
         Task {
-            //            await gameCenterManager.submitPoint(point: Int(weeklyNfcPoint) + Int(weeklyStairPoint))
-            await gameCenterManager.submitPoint(point: 1840)
+//                        await gameCenterManager.submitPoint(point: Int(weeklyNfcPoint) + Int(weeklyStairPoint))
+            await gameCenterManager.submitPoint(point: 2128)
         }
     }
     
