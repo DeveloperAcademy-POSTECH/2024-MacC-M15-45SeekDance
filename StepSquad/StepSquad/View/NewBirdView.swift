d58e8c1258f16324f2d788b9a380ec24d508aae2
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2025-01-22 12:10:25 +0900

    #162 feat: create NewBirdView

---
+++ StepSquad/StepSquad/View/NewBirdView.swift
@@ -0,0 +1,19 @@
+//
+//  NewBirdView.swift
+//  StepSquad
+//
+//  Created by Groo on 1/21/25.
+//
+
+import SwiftUI
+
+struct NewBirdView: View {
+    var body: some View {
+        ZStack {
+        }
+    }
+}
+
+#Preview {
+    NewBirdView()
+}
