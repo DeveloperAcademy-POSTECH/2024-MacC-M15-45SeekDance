761d09222c5b899119c263a6d88c968d050377df
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2025-01-23 11:59:40 +0900

    #162 feature: create 'n일 동안 n층을 ~ 하산했어요!(컨페티 나올 뷰)' view

--- StepSquad/StepSquad/View/NewBirdView.swift
+++ StepSquad/StepSquad/View/NewBirdView.swift
@@ -10,6 +10,29 @@ import SwiftUI
 struct NewBirdView: View {
     var body: some View {
         ZStack {
+            VStack {
+                Spacer()
+                VStack {
+                    HStack(spacing: 0) {
+                        Text("\(123)")
+                            .foregroundStyle(Color(hex: 0x7EB55D))
+                        Text("일 동안")
+                        Text(" \(1449)")
+                            .foregroundStyle(Color(hex: 0x7EB55D))
+                        Text("층을")
+                    }
+                    Text("함께 오른 틈새가")
+                    Text("하산을 했어요!")
+                }
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                    .fontWeight(.bold)
+                Image("Down1")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 256)
+                Spacer()
+            }
         }
     }
 }
