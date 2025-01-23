d9642acb4773e454658c522aa8d2ea31c2bc16aa
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2025-01-23 14:04:01 +0900

    #169 feature: create RiveExampleView but 'no such module 'RiveRuntime''

---
+++ StepSquad/StepSquad/View/RiveExampleView.swift
@@ -0,0 +1,24 @@
+//
+//  RiveExampleView.swift
+//  StepSquad
+//
+//  Created by Groo on 1/23/25.
+//
+
+import SwiftUI
+import RiveRuntime
+
+struct RiveExampleView: View {
+    var body: some View {
+        VStack {
+            RiveViewModel(fileName: "cool_rive_animation").view()
+            RiveViewModel(
+                webURL: "https://cdn.rive.app/animations/off_road_car_v7.riv"
+            ).view()
+        }
+    }
+}
+
+#Preview {
+    RiveExampleView()
+}
