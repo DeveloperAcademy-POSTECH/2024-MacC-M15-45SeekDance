8643038b69b71e09d8ceefc07cc6b5070bf75f7e
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2025-01-29 12:59:24 +0900

    #174 feat: rename file, create NewBirdView and show temporary

--- StepSquad/StepSquad/View/ShowNewBirdView.swift
+++ StepSquad/StepSquad/View/ShowNewBirdView.swift
@@ -7,12 +7,14 @@
 
 import SwiftUI
 import EffectsLibrary
+import RiveRuntime
 
-struct NewBirdView: View {
+struct ShowNewBirdView: View {
     @State var timeCount = 0
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @State var opacity1 = 1.0
     @State var opacity2 = 0.0
+    @State var opacity3 = 0.0
     var body: some View {
         ZStack {
             ItemsConfettiView()
@@ -21,19 +23,25 @@ struct NewBirdView: View {
             EmptyNestView()
                 .opacity(opacity2)
                 .animation(.linear(duration: 1), value: opacity2)
+            NewBirdView()
+                .opacity(opacity3)
+                .animation(.linear(duration: 1), value: opacity3)
         }
         .onReceive(timer) { _ in
             timeCount += 1
-            if timeCount == 5 {
+            if timeCount == 1 {
                 opacity1 = 0.0
                 opacity2 = 1.0
+            } else if timeCount == 8 {
+                opacity2 = 0.0
+                opacity3 = 1.0
             }
         }
     }
 }
 
 #Preview {
-    NewBirdView()
+    ShowNewBirdView()
 }
 
 extension View {
@@ -116,6 +124,44 @@ struct ItemsConfettiView: View {
 
 struct EmptyNestView: View {
     var body: some View {
-        Text("Empty Nest")
+        VStack {
+            ZStack(alignment: .top) {
+                VStack {
+                    Text("틈새가 떠나")
+                    Text("빈 둥지가 되었어요")
+                }
+                .font(.title)
+                .fontWeight(.bold)
+                Text("앗 뭐지??")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color(hex: 0x638D48))
+            }
+            RiveViewModel(fileName: "reset").view()
+        }
+    }
+}
+
+struct NewBirdView: View {
+    var body: some View {
+        VStack {
+            VStack {
+                Text("하산한 틈새가 떠나고")
+                HStack(spacing: 0) {
+                    Text("새로운 틈새")
+                        .foregroundStyle(Color(hex: 0x7EB55D))
+                    Text("가 왔어요!")
+                }
+            }
+            .font(.title)
+            .fontWeight(.bold)
+            Text("같은 틈새 아닌가 하는 마음이 든다면 그건 착각이에요")
+                .frame(width: 256)
+            Image("ResultIMG")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 256)
+        }
+        .multilineTextAlignment(.center)
     }
 }
