9998718526edb4a5df8b4f62d5b6d7595027b412
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2025-02-27 10:21:29 +0900

    #191 feat: add more property into GPSStaircase

--- StepSquad/StepSquad/Resource/GPSStaircaseModel.swift
+++ StepSquad/StepSquad/Resource/GPSStaircaseModel.swift
@@ -20,32 +20,33 @@ enum KoreanProvince: String, Codable {
 }
 
 class GPSStaircase: Codable, Identifiable {
-    let id: String // TODO: id는 영문명으로 계단 이미지도 id와 동일하게 설정
-    let name: String
-    let title: String
-    let steps: Int
-    let locationX: Int // TODO: 인증방식에 따라 Location으로 변경
-    let locationY: Int
-    let province: KoreanProvince
-    let description: String
-    let reward: String // TODO: 리워드 이미지 파일명은 "[id]_reward"로 설정
+    let id: String // 영문명 TODO: 계단 이미지도 id와 동일하게 설정
+    let name: String // 계단명
+    let title: String // 계단의 부제
+    let steps: Int // 계단 개수
+    let latitude: Double // 위도
+    let longitude: Double // 경도
+    let province: KoreanProvince // 해당 지역
+    let description: String // 설명
+    let verificationLocation: String // 인증 위치 TODO: 인증 위치 파일명은 "[id]_location"
+    let reward: String // 획득 재료 TODO: 리워드 이미지 파일명은 "[id]_reward"로 설정
     let achievementId: String
     
-    init(id: String, name: String, title: String, steps: Int, location: (Int, Int), province: KoreanProvince, description: String, reward: String, achievementId: String) {
+    init(id: String, name: String, title: String, steps: Int, latitude: Double, longitude: Double, province: KoreanProvince, description: String, verificationLocation: String, reward: String, achievementId: String) {
         self.id = id
         self.name = name
         self.title = title
         self.steps = steps
-        self.locationX = location.0
-        self.locationY = location.1
+        self.latitude = latitude
+        self.longitude = longitude
         self.province = province
         self.description = description
+        self.verificationLocation = verificationLocation
         self.reward = reward
         self.achievementId = achievementId
     }
 }
 
 let localStaircases: [GPSStaircase] = [
-    GPSStaircase(id: "Test1", name: "동네 계단", title: "우리 동네에 이런 계단이 있다고?", steps: 70, location: (1, 2), province: .jeju, description: "네 있습니다. 참고 설명입니다.", reward: "굉장한 선물", achievementId: "test1"),
-    GPSStaircase(id: "Test2", name: "먼 계단", title: "먼 동네에 이런 계단이 있다고?", steps: 70, location: (1, 2), province: .jeju, description: "네 있습니다. 참고 설명입니다.", reward: "굉장히 선물", achievementId: "test2"),
+//    GPSStaircase(id: "Test1", name: "동네 계단", title: "우리 동네에 이런 계단이 있다고?", steps: 70, latitude: <#T##Double#>, province: .jeju, description: "네 있습니다. 참고 설명입니다.", reward: "굉장한 선물", achievementId: "test1"),
 ]
