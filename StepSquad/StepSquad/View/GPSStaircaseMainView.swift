597a041b12c81cafda17adcc71cfa0e437e789c4
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-05-06 11:42:25 +0900

    #208 feat: adjust size of header

--- StepSquad/StepSquad/View/GPSStaircaseMainView.swift
+++ StepSquad/StepSquad/View/GPSStaircaseMainView.swift
@@ -9,43 +9,47 @@ import SwiftUI
 
 struct GPSStaircaseMainView: View {
     var body: some View {
-        VStack {
-            ZStack {
-                // TODO: 캐러셀 이미지, 애니메이션 추가
-                
-                VStack {
-                    Spacer()
-                    LinearGradient(stops: [.init(color: Color(hex: 0x16240E), location: 0), .init(color: .clear, location: 1)], startPoint: .bottom, endPoint: .top)
-                        .frame(height: 142)
-                }
-                
-                VStack {
-                    Image("ribbon")
-                        .resizable()
-                        .frame(width: 312, height: 100)
-                        .padding(.top, 24)
+        ScrollView {
+            VStack {
+                ZStack {
+                    // TODO: 캐러셀 이미지, 애니메이션 추가
                     
-                    Spacer()
+                    VStack {
+                        Spacer()
+                        LinearGradient(stops: [.init(color: Color(hex: 0x16240E), location: 0), .init(color: .clear, location: 1)], startPoint: .bottom, endPoint: .top)
+                            .frame(height: 142)
+                    }
                     
-                    Button(action: {
-                        // TODO: 전국의 계단 리스트로 이동
-                    }, label: {
-                        HStack {
-                            Text("전국의 계단 리스트 보기")
-                                .foregroundStyle(.white)
-                                .bold()
-                        }
-                        .frame(width: 248, height: 50)
-                        .background(RoundedRectangle(cornerRadius: 10).fill(.green500))
-                    })
-                    .padding(.bottom, 40)
+                    VStack {
+                        Image("ribbon")
+                            .resizable()
+                            .frame(width: 312, height: 100)
+                            .padding(.top, 122)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // TODO: 전국의 계단 리스트로 이동
+                        }, label: {
+                            HStack {
+                                Text("전국의 계단 리스트 보기")
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                            .frame(width: 248, height: 50)
+                            .background(RoundedRectangle(cornerRadius: 10).fill(.green500))
+                        })
+                        .padding(.bottom, 40)
+                    }
                 }
+                .frame(height: 444)
+                .frame(maxWidth: .infinity)
+                .background(.green200)
+                
+                Spacer()
             }
-            .frame(height: 444)
-            .frame(maxWidth: .infinity)
-            .background(.green200)
-            Spacer()
         }
+        .ignoresSafeArea()
     }
 }
 
