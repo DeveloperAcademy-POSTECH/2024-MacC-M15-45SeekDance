584e82e28c90bf96a2ee9d7e60116d3a2ce60903
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-05-19 11:10:03 +0900

    #208 feat: link MissionDetailView

--- StepSquad/StepSquad/View/GPSStaircaseListView.swift
+++ StepSquad/StepSquad/View/GPSStaircaseListView.swift
@@ -21,78 +21,80 @@ struct GPSStaircaseListView: View {
         ScrollView {
             LazyVGrid(columns: columns, spacing: 8) {
                 ForEach(filteredStaircases) { staircase in
-                    VStack(alignment: .leading, spacing: 12) {
-                        // TODO: 인증 과정 구현 후 삭제
-//                        Button("임시 인증/해제") {
-//                            if collectedItems.isCollected(item: staircase.id) {
-//                                collectedItems.deleteItem(item: staircase.id)
-//                            } else {
-//                                collectedItems.collectItem(item: staircase.id, collectedDate: Date.now)
-//                            }
-//                        }
-                        
-                        Text(staircase.title)
-                            .font(.footnote)
-                            .bold()
-                            .foregroundStyle(.green700)
-                            .frame(maxWidth: .infinity)
-                            .multilineTextAlignment(.center)
-                        
-                        ZStack {
-                            Image(staircase.imageName)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 124, height: 108)
-                                .clipShape(Rectangle())
+                    NavigationLink(destination: MissionDetailView(), label: {
+                        VStack(alignment: .leading, spacing: 12) {
+                            // TODO: 인증 과정 구현 후 삭제
+    //                        Button("임시 인증/해제") {
+    //                            if collectedItems.isCollected(item: staircase.id) {
+    //                                collectedItems.deleteItem(item: staircase.id)
+    //                            } else {
+    //                                collectedItems.collectItem(item: staircase.id, collectedDate: Date.now)
+    //                            }
+    //                        }
                             
-                            if collectedItems.isCollected(item: staircase.id) {
-                                Rectangle()
-                                    .foregroundStyle(.black)
-                                    .opacity(0.4)
-                                    .frame(width: 124, height: 108)
-                                
-                                Text("방문 완료")
-                                    .font(.footnote)
-                                    .bold()
-                                    .foregroundStyle(.white)
-                            }
-                        }
-                        
-                        HStack {
-                            Text(staircase.name)
+                            Text(staircase.title)
                                 .font(.footnote)
                                 .bold()
-                            Spacer()
-                            Button(action: {
-                                if bookmarks.contains(staircase.id) {
-                                    bookmarks.remove(staircase.id)
-                                } else {
-                                    bookmarks.add(staircase.id)
-                                }
-                            }, label: {
-                                Image(systemName: bookmarks.contains(staircase.id) ? "bookmark.fill" : "bookmark")
-                                    .font(.caption)
-                                    .foregroundStyle(bookmarks.contains(staircase.id) ? .brown500 : .grey400)
-                            })
-                        }
-                        
-                        HStack {
-                            Text(staircase.province.rawValue)
-                                .font(.caption)
-                                .foregroundStyle(.white)
-                                .padding(4)
-                                .background(RoundedRectangle(cornerRadius: 4).fill(.green600))
+                                .foregroundStyle(.green700)
+                                .frame(maxWidth: .infinity)
+                                .multilineTextAlignment(.center)
                             
-                            Text("\(staircase.steps)칸")
-                                .font(.caption)
-                                .foregroundStyle(.green800)
-                                .padding(4)
-                                .background(RoundedRectangle(cornerRadius: 4).fill(.green50))
+                            ZStack {
+                                Image(staircase.imageName)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 124, height: 108)
+                                    .clipShape(Rectangle())
+                                
+                                if collectedItems.isCollected(item: staircase.id) {
+                                    Rectangle()
+                                        .foregroundStyle(.black)
+                                        .opacity(0.4)
+                                        .frame(width: 124, height: 108)
+                                    
+                                    Text("방문 완료")
+                                        .font(.footnote)
+                                        .bold()
+                                        .foregroundStyle(.white)
+                                }
+                            }
+                            
+                            HStack {
+                                Text(staircase.name)
+                                    .font(.footnote)
+                                    .bold()
+                                Spacer()
+                                Button(action: {
+                                    if bookmarks.contains(staircase.id) {
+                                        bookmarks.remove(staircase.id)
+                                    } else {
+                                        bookmarks.add(staircase.id)
+                                    }
+                                }, label: {
+                                    Image(systemName: bookmarks.contains(staircase.id) ? "bookmark.fill" : "bookmark")
+                                        .font(.caption)
+                                        .foregroundStyle(bookmarks.contains(staircase.id) ? .brown500 : .grey400)
+                                })
+                            }
+                            
+                            HStack {
+                                Text(staircase.province.rawValue)
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .padding(4)
+                                    .background(RoundedRectangle(cornerRadius: 4).fill(.green600))
+                                
+                                Text("\(staircase.steps)칸")
+                                    .font(.caption)
+                                    .foregroundStyle(.green800)
+                                    .padding(4)
+                                    .background(RoundedRectangle(cornerRadius: 4).fill(.green50))
+                            }
                         }
-                    }
-                    .frame(width: 124, height: 220)
-                    .padding(12)
-                    .background(RoundedRectangle(cornerRadius: 4).fill(.white))
+                        .frame(width: 124, height: 220)
+                        .padding(12)
+                        .background(RoundedRectangle(cornerRadius: 4).fill(.white))
+                    })
                 }
             }
         }
