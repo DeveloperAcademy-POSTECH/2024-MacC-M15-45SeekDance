95002c51eab415101e0730738db1c64cd4c0fef7
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-05-24 18:53:06 +0900

    #214 design: set temporal design

--- StepSquad/StepSquad/View/TestLocationVerificationView.swift
+++ StepSquad/StepSquad/View/TestLocationVerificationView.swift
@@ -11,13 +11,19 @@ struct TestLocationVerificationView: View {
     @State var location = ""
     let locationManager = LocationManager()
     var body: some View {
-        Button("위치 권한 받기") {
-            locationManager.requestWhenInUseAuthorization()
+        VStack(spacing: 30) {
+            Button("위치 권한 받기") {
+                locationManager.requestWhenInUseAuthorization()
+            }
+            .buttonStyle(.borderedProminent)
+            Button("위치 확인") {
+                location = locationManager.verifyLocation()
+            }
+            .buttonStyle(.bordered)
+            Text(location)
+                .multilineTextAlignment(.center)
+                .font(.title2)
         }
-        Button("위치 확인") {
-            location = locationManager.verifyLocation()
-        }
-        Text(location)
     }
 }
 
