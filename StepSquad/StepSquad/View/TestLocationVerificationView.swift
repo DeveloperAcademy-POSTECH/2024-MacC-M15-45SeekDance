47ff0cd477e96ba0436d8066c849b5b5ace4d6f2
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-07-05 15:49:13 +0900

    #214 test: set location manager with withCheckedThrowingContinuation

--- StepSquad/StepSquad/View/TestLocationVerificationView.swift
+++ StepSquad/StepSquad/View/TestLocationVerificationView.swift
@@ -6,32 +6,68 @@
 //
 
 import SwiftUI
+import CoreLocation
+import CoreLocationUI
 
-struct TestLocationVerificationView: View {
-    @State var location = ""
-    let locationManager = LocationManager()
-    var body: some View {
-        VStack(spacing: 30) {
-            Button("위치 권한 받기") {
-                print("위치 권한 버튼")
-                locationManager.requestAlwaysAuthorization()
-            }
-            .buttonStyle(.borderedProminent)
-            Button("위치 확인") {
-                print("위치 권한 버튼")
-                location = locationManager.testLocation()
-            }
-            .buttonStyle(.bordered)
-            Text(location)
-                .multilineTextAlignment(.center)
-                .font(.title2)
+/// 지연된 Continuation 처리를 위한 간단한 코드 스니펫<br>
+/// 위치 관리자
+class LocationManagerTester: NSObject, ObservableObject, CLLocationManagerDelegate {
+    // 위치 값을 내어주는 CheckedContinuation
+    private var locationContinuation: CheckedContinuation<CLLocationCoordinate2D?, Error>?
+    // 코어 위치 관리자
+    private let manager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() async throws -> CLLocationCoordinate2D? {
+        try await withCheckedThrowingContinuation { continuation in
+            locationContinuation = continuation
+            manager.requestLocation()
+            // continuation 을 resume 시키지 않는 한, 비동기 블럭을 대기하고 있는다.
         }
-        .onAppear() {
-            print("onAppear")
-        }
+    }
+    
+    //  CLLocationManagerDelegate - 현재 위치 획득
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Continuation 에 위치정보를 전달하면서 멈춰있던 실행을 진행시킨다.
+        locationContinuation?.resume(returning: locations.first?.coordinate)
+    }
+    // CLLocationManagerDelegate - 위치 찾기 실패
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // Continuation 에 오류를 전달하면서 멈춰있던 실행을 진행시킨다.
+        locationContinuation?.resume(throwing: error)
     }
 }
 
-#Preview {
-    TestLocationVerificationView()
+
+struct TestLocationVerificationView: View {
+    @State private var manager = LocationManagerTester()
+    @State private var currentLocation: CLLocationCoordinate2D?
+    var body: some View {
+        VStack {
+            if let currentLocation = currentLocation {
+                Text(currentLocation.latitude.description + ", " + currentLocation.longitude.description)
+                    .font(.title)
+            } else {
+                ProgressView()
+            }
+            Text(currentLocation?.longitude.description ?? "No location yet.")
+            LocationButton {
+                Task {
+                    currentLocation = nil
+                    if let location = try? await manager.requestLocation() {
+                        print("Location: \(location)")
+                        currentLocation = location
+                    } else {
+                        print("Location unknown.")
+                    }
+                }
+            }
+            .clipShape(Capsule())
+            .padding()
+        }
+    }
 }
