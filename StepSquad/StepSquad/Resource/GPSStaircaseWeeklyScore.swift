ebee7b157d3e822c1a05667a2ce425fcd0faca88
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-07-16 19:26:17 +0900

    #214 feat: change dateFormatter and save it after edit also fix save function

--- StepSquad/StepSquad/Resource/GPSStaircaseWeeklyScore.swift
+++ StepSquad/StepSquad/Resource/GPSStaircaseWeeklyScore.swift
@@ -10,7 +10,7 @@ import Foundation
 @Observable
 class GPSStaircaseWeeklyScore: Codable {
     private var scores: [String: Int]
-    private let savePath = URL.documentsDirectory.appending(path: "GPSStaircaseWeekScore")
+    private let savePath = URL.documentsDirectory.appending(path: "GPSStaircaseWeeklyScore")
     
     init() {
         do {
@@ -24,27 +24,18 @@ class GPSStaircaseWeeklyScore: Codable {
     
     // MARK: 특정 계단 인증 완료 후 이번주 점수에 더하기
     func addScore(score: Int, todayDate: Date = Date.now) {
-        print("addScore")
-        print("before scores: \(scores)")
-        let dateFormatter: DateFormatter = .init()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        dateFormatter.timeStyle = .none
         let formattedTodayString = dateFormatter.string(from: todayDate)
-        print("todayDate: \(todayDate)")
-        print("formattedTodayString: \(formattedTodayString)")
-        
         if let dayScore = scores[formattedTodayString] {
             scores[formattedTodayString]! += score // 오늘 기록이 있을 때
         } else {
             scores[formattedTodayString] = score // 오늘 기록이 없을 때
         }
-        
-        print("after scores: \(scores)")
+        save()
+        print("saved scores: \(scores)")
     }
     
     // MARK: 이번주 점수 계산 후 반환
     func getWeeklyScore() -> Int {
-        print("getWeeklyScore")
         cleanOutdatedScores()
         var totalScore = 0
         for dayScore in scores {
@@ -56,9 +47,6 @@ class GPSStaircaseWeeklyScore: Codable {
     // MARK: 이번주가 아닌 점수 삭제
     func cleanOutdatedScores(todayDate: Date = Date.now) {
         print("cleanOutdatedScores")
-        let dateFormatter: DateFormatter = .init()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        dateFormatter.timeStyle = .none
         let formattedTodayString = dateFormatter.string(from: todayDate)
         let today = Calendar.current.component(.weekday, from: todayDate) // 오늘의 요일, 일요일 = 1 ~ 토요일 = 7
         print("today: \(today)")
@@ -71,7 +59,8 @@ class GPSStaircaseWeeklyScore: Codable {
         } else {
             for (key, _) in scores {
                 guard let targetDate = dateFormatter.date(from: key) else {
-                    print("GPSStaircaseWeekScore: targetDate 설정이 잘못되었습니다.")
+                        scores.removeValue(forKey: key)
+                    print("GPSStaircaseWeeklyScore: 잘못된 key(\(key)) 삭제")
                     return
                 }
                 if let gap = Calendar.current.dateComponents([.day], from: targetDate, to: todayDate).day {
@@ -83,12 +72,14 @@ class GPSStaircaseWeeklyScore: Codable {
             }
             print("scores: \(scores)")
         }
+        save()
     }
     
     func save() {
         do {
-            let data = try JSONEncoder().encode(levels)
+            let data = try JSONEncoder().encode(scores)
             try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            print("save 완료")
         } catch {
             print("Unable to save GPSStaircaseWeekScore.")
         }
