184dc87e7dbb6c8ab33b6805e716629900cad020
soaringwave <82134672+grootwo@users.noreply.github.com>
2025-10-12 17:20:49 +0900

    #231 feat: set NotificationManager

---
+++ StepSquad/StepSquad/Helper/NotificationManager.swift
@@ -0,0 +1,59 @@
+//
+//  NotificationManager.swift
+//  StepSquad
+//
+//  Created by Groo on 10/12/25.
+//
+
+import Foundation
+import UserNotifications
+import CoreLocation
+
+class NotificationManager {
+    static let instance = NotificationManager()
+    
+    func requestAuthorization() {
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { suceess, error in
+            if error != nil {
+                print("🚩ERROR: denied notification.")
+            } else {
+                print("🚩noti center: authorized")
+                self.requestLocationTriggerNotification()
+            }
+        }
+    }
+    
+    func requestLocationTriggerNotification() {
+        UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: ["gpsStaircaseLocationNotification"]) // 이전 위치 알림 삭제
+        
+        for gpsStaircase in gpsStaircases {
+            let content = UNMutableNotificationContent()
+            content.title = "주변에 계단 명소가?!"
+            content.subtitle = "근처의 \(gpsStaircase.name)에 방문하여 미션을 달성해보는 건 어떤가요?"
+            content.sound = .default
+            content.badge = 1
+            
+            let coordinates = CLLocationCoordinate2D(
+                latitude: gpsStaircase.latitude,
+                longitude: gpsStaircase.longitude)
+            let region = CLCircularRegion(
+                center: coordinates,
+                radius: 3000,
+                identifier: "gpsStaircaseLocation")
+            region.notifyOnEntry = true
+            region.notifyOnExit = false
+            let trigger = UNLocationNotificationTrigger(region: region, repeats: true)
+            
+            let request = UNNotificationRequest(
+                identifier: "gpsStaircaseLocationNotification",
+                content: content,
+                trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request)
+            
+            print("noti center: \(gpsStaircase.name) request added")
+        }
+        print("🚩noti center: completed request")
+    }
+}
